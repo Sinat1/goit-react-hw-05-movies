bde7622c674b7a281533e906c0d3aab83309021e
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2022-11-29 13:02:06 +0200

    add bug fix
    -add correct backtrack to trending movies(home);
    -add correct homepage location on github-pages;

--- src/components/TrendingMovies/TrendingMoviesList.jsx
+++ src/components/TrendingMovies/TrendingMoviesList.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const TrendingMoviesList = ({ trendingMovies }) => {
+  const location = useLocation();
+
   return (
     <ul>
       {trendingMovies.map(movie => (
         <li key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
             {movie.title}
           </Link>
         </li>
